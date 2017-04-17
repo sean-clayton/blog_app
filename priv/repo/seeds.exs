1569741c16034f8ccc5c37ad1e7fcc9963501383
Sean Clayton 🎷🐢 <sclayton@swifttrip.com>
2017-04-17 16:42:01 -0400

    Create users and posts via seed

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,26 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias BlogApp.Repo
+alias BlogApp.Accounts.User
+alias BlogApp.Blog.Post
+
+# Create 10 seed users
+
+for _ <- 1..10 do
+  Repo.insert!(%User{
+    name: Faker.Name.name,
+    email: Faker.Internet.safe_email
+  })
+end
+
+# Create 40 seed posts
+
+for _ <- 1..40 do
+  Repo.insert!(%Post{
+    title: Faker.Lorem.sentence,
+    body: Faker.Lorem.sentences(%Range{first: 1, last: 3}) |> Enum.join("\n\n"),
+    accounts_users_id: Enum.random(1..10) # Pick random user for post to belong to
+  })
+end
